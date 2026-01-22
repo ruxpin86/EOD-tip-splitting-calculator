ddb94d6db2741803f29f417d4b6ad4c99903017e
Ted G <will.glynn@gmail.com>
2026-01-21 21:15:11 -0700

    We have output back to the page once the Python script is run through the main script, holy smokes! Alright now I have to make this look nice and readable because right now it looks like a total mess. Also Jinja is really weird syntax inside of HTML.

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    results = None
     if request.method == "POST":
         raw_text = request.form["employees"] #get the input from the page and conver it to a string
         emp_dict = parse_employee_data(raw_text) #parse the raw_text into a form that is usable by the calculator
         total_tips = float(request.form["total_tips"]) #grab the total_tips input from the page
         results = calculate_tip_share(emp_dict, total_tips) #calculate the tip out
     
-    return render_template("index.html")
+    return render_template("index.html", payouts=results)
 
 def parse_employee_data(text):
     employee_info = {} #dictionary that will be populated with employee data
